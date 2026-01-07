b22a0b93c231ce04b23769595c24bd8b98f150b6
huandreg <jest40k@gmail.com>
2026-01-07 13:41:31 +0100

    remélem ez sajátba commit-ol

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,25 @@
+import { useEffect, useState } from "react";
+
+export type PayStructure = {
+  name: string,
+  pricePerMonth: number,
+  features: string[];
+}
+
 const App = () => {
+
+
+  const [payStructures, getStructures] = useState<PayStructure[]>([])
+
+  useEffect(() => {
+    fetch("data.json")
+      .then(beJson => beJson.json())
+      .then(toData => getStructures(toData))
+  }, [])
+
+
   return (
-    <div>App</div>
+    <div></div>
   )
 }
 
--- src/components/PriceCard.tsx
+++ src/components/PriceCard.tsx
@@ -1,3 +1,5 @@
+import type {PayStructure} from "../App"
+
 const PriceCard = () => {
   return (
     <div>PriceCard</div>
