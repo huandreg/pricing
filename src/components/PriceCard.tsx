74408dc86ba7cce4d84b25339c9382a4dc23dfd8
huandreg <jest40k@gmail.com>
2026-01-08 09:41:13 +0100

    working without yearly context

--- src/components/PriceCard.tsx
+++ src/components/PriceCard.tsx
@@ -1,13 +1,16 @@
 import type { PayStructure } from "../App"
 
 const PriceCard = (props: PayStructure) => {
+  function buttonClick(prop:string) {
+    alert(prop + " option choosen!");
+  }
   return (
 
     <div className={props.highlighted ? "whole-card highlighted" : "whole-card"}>
       <h2>{props.name}</h2>
-      <h1>{props.pricePerMonth === 0 ? "Free" : "$" + props.pricePerMonth }</h1>
-      <div className="feature-div">{props.features.map((item)=>{return <h4>{item}</h4>})}</div>
-      <div className="try-button">Try for Free</div>
+      <h1>{props.pricePerMonth === 0 ? "Free" : "$" + props.pricePerMonth}</h1>
+      <div className="feature-div">{props.features.map((item) => { return <h4>{item}</h4> })}</div>
+      <div className="try-button" key={props.name} onClick={()=>buttonClick(props.name)}>Try for Free</div>
     </div>
   )
 }
